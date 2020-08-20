9f057e0c243155987196b55d31aa904214cc27e6
Nicolás Esteban Olmedo <olmedoni@hotmail.com>
2020-08-20 18:38:29 -0300

    Más ejercicios

---
+++ asignaciones_y_variables_07.c
@@ -0,0 +1,68 @@
+/*
+*
+* Título: EJERCICIOS DE VARIABLES Y ASIGNACIONES 07
+*	Consigna: Ingresar dos valores, calcular su suma, su producto y la resta del 1ro menos el 2do.
+*	Alumno: Nicolás Esteban Olmedo
+*
+*/
+
+/* Directivas del preprocesador */
+#include <stdio.h>
+
+/* Macros del procesador */
+
+/* Definiciones globales */
+	/* Declaración de variables globales */
+	
+	/* Declaración de prototipos de funciones */
+void clrscr(void);
+float suma(float arg0, float arg1);
+float resta(float arg0, float arg1);
+float producto(float arg0, float arg1);
+
+/* Función principal */
+int main() {
+	/* Declaración de variables locales */
+	float valor0, valor1, res_suma, res_resta, res_prod;
+	
+	/* Input de data */
+	printf("Ingresar un valor para A: ");
+	scanf("%f", &valor0);
+	printf("Ingresar un valor para B: ");
+	scanf("%f", &valor1);
+	
+	/* Procesamiento de data */
+	res_suma = suma(valor0, valor1);
+	res_resta = resta(valor0, valor1);
+	res_prod = producto(valor0, valor1);
+	
+	/* Output de data */
+	printf("A + B = %f\n B - A = %f\n A * B = %f\n", res_suma, res_resta, res_prod);
+	return 0;	
+}
+
+/* Otras funciones */
+	/* Declaración de clrscr() */
+void clrscr(void) {
+	system("clear");
+}
+	/* Declaración de suma() */
+float suma(float arg0, float arg1) {
+	float res;
+	res = arg0 + arg1;
+	return res;
+}
+
+	/* Declaración de resta() */
+float resta(float arg0, float arg1) {
+	float res;
+	res = arg1 - arg0;
+	return res;
+}
+
+ /* Declaración de producto */ 
+float producto(float arg0, float arg1) {
+	float res;
+	res = arg0 * arg1;
+	return res;
+}
