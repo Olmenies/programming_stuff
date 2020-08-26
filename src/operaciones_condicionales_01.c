4a4cf8400ae02574a0493007a1aaf7939125b8dc
Nicolás Esteban Olmedo <olmedoni@hotmail.com>
2020-08-26 13:14:33 -0300

    Reestructuracion de ejercicios

---
+++ src/operaciones_condicionales_01.c
@@ -0,0 +1,41 @@
+/*
+*
+*	Título: EJERCICIOS DE OPERACIONES CONDICIONALES
+*	Consigna: Ingresar dos valores, indicar si son iguales.
+*	Alumno: Nicolás Esteban Olmedo
+*
+*/
+
+/* Directivas del preprocesador */
+#include <stdio.h>
+
+/* Macros del procesador */
+/* Declaraciones globales */
+	/* Declaración de variables globales */
+	/* Declaración de prototipos de funciones */
+void clrscr(void);
+
+/* Función principal */
+int main()
+{
+	clrscr();
+	/* Declaraciones locales */
+	int valor0, valor1;
+	
+	/* Input de data */
+	printf("Ingresar dos valores: ");
+	scanf("%d", valor0, valor1);
+	
+	/* Procesamiento de data */
+	/* Output de data */
+		if(valor0 == valor1)
+			puts("Los valores ingresados son iguales");
+		return 0;
+}
+
+/* Otras funciones */
+	/* Definición de clrscr() */
+void clrscr(void)
+{
+	system("clear");
+}
